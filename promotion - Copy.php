254d34df7f76f430f2e9c4c560009ea6a6b0edd4
nitikarn <nitikarnboom2030@gmail.com>
2015-12-28 12:20:15 +0700

    add

---
+++ promotion - Copy.php
@@ -0,0 +1,101 @@
+<div id="allPromotion">
+    <img src="<?php echo ADDRESS_MEDIA ?>โปรโมชั่นเฉพาะเดือน.png"  style="width:100%;"/>
+    <table border="0" cellpadding="5" cellspacing="0" class="container-box" >
+        <tbody>
+            <tr>
+                <td valign="top"><table  border="0" cellspacing="0" cellpadding="5">
+                        <tbody>
+                            <tr>
+                                <td><center>
+                            <table border="0">
+                                <tbody>
+                                    <tr>
+
+                                        <th align="center" bgcolor="#9eda67" width="300">ชื่อโปรโมชั่น</th>
+                                        <th align="center" bgcolor="#9eda67" width="300">จุดเด่น</th>
+                                        <th align="center" bgcolor="#9eda67" width="139"><span style="color: #ff0000">IRR สูงสุด</span></th>
+                                        <th align="center" bgcolor="#9eda67" width="132"><span style="color: #000">ระยะเวลาชำระเบี้ย</span></th>
+                                        <th align="center" bgcolor="#9eda67" width="132"><span style="color:#000;">ระยะเวลาเอาประกัน</span></th>
+                                       
+                                        <th align="center" bgcolor="#9eda67" width="99">Status</th>
+                                        <th align="center" bgcolor="#9eda67" width="99">รายละเอียด</th>
+                                    </tr>
+                                  
+                                    <tr style="background-color:#EEEEEE;">
+
+                                        <td align="left"><a href="#" style="color:#FF0099;" title="เมืองไทยซุปเปอร์กู๊ด 12/5<">เมืองไทยซุปเปอร์กู๊ด 12/5</a></td>
+                                        <td align="left">- ดอกเบี้ยสูงกว่าธนาคาร <br>- รับเงินทุกปี สูงสุด 10% <br>- นำไปลดหย่อนภาษีได้</td>
+                                        <td align="center">3.25% (ปลอดภาษี)</td>
+                                        <td align="center">5 ปี</td>
+                                        <td align="center">12 ปี</td>
+                                      
+                                        <td align="center"><h3>คงอยู่</h3></td>
+                                        <th align="center" bgcolor="#9eda67" width="99"><a href="<?php echo ADDRESS ?>แบบประกันยอดนิยม/เมืองไทยซุปเปอร์กู๊ด" class="btnSee">คลิกดู</a></th>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </center></td>
+            </tr>
+        </tbody>
+    </table></td>
+</tr>
+</tbody>
+</table>
+
+</div>
+<br />
+<br />
+
+<style>
+    #allPromotion{
+        -webkit-border-radius: 10px;
+        -moz-border-radius: 10px;
+        border-radius: 10px;
+        -webkit-box-shadow: inset 0 0 20px rgba(242, 16, 172, 0.73);
+        -moz-box-shadow: inset 0 0 20px rgba(242, 16, 172, 0.73);
+        box-shadow: inset 0 0 20px rgba(242, 16, 172, 0.73);
+    }
+    th{font-size:14px;}
+    .btnSee {
+        -moz-box-shadow:inset 0px 1px 0px 0px #fbafe3;
+        -webkit-box-shadow:inset 0px 1px 0px 0px #fbafe3;
+        box-shadow:inset 0px 1px 0px 0px #fbafe3;
+        background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ff5bb0), color-stop(1, #ef027d));
+        background:-moz-linear-gradient(top, #ff5bb0 5%, #ef027d 100%);
+        background:-webkit-linear-gradient(top, #ff5bb0 5%, #ef027d 100%);
+        background:-o-linear-gradient(top, #ff5bb0 5%, #ef027d 100%);
+        background:-ms-linear-gradient(top, #ff5bb0 5%, #ef027d 100%);
+        background:linear-gradient(to bottom, #ff5bb0 5%, #ef027d 100%);
+        filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff5bb0', endColorstr='#ef027d',GradientType=0);
+        background-color:#ff5bb0;
+        -moz-border-radius:6px;
+        -webkit-border-radius:6px;
+        border-radius:6px;
+        border:1px solid #ee1eb5;
+        display:inline-block;
+        cursor:pointer;
+        color:#ffffff !important;
+        font-family:arial;
+        font-size:13px;
+        font-weight:bold;
+        padding:6px 24px;
+        text-decoration:none !important;
+        text-shadow:0px 1px 0px #c70067;
+    }
+    .btnSee:hover {
+        background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ef027d), color-stop(1, #ff5bb0));
+        background:-moz-linear-gradient(top, #ef027d 5%, #ff5bb0 100%);
+        background:-webkit-linear-gradient(top, #ef027d 5%, #ff5bb0 100%);
+        background:-o-linear-gradient(top, #ef027d 5%, #ff5bb0 100%);
+        background:-ms-linear-gradient(top, #ef027d 5%, #ff5bb0 100%);
+        background:linear-gradient(to bottom, #ef027d 5%, #ff5bb0 100%);
+        filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ef027d', endColorstr='#ff5bb0',GradientType=0);
+        background-color:#ef027d;
+    }
+    .btnSee:active {
+        position:relative;
+        top:1px;
+    }
+
+</style>
